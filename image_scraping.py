d9c3ec1488a1ee6d7d222c97807ccb0e81246f74
Kushgulati09 <129382527+Kushgulati09@users.noreply.github.com>
2024-01-09 00:27:58 +0530

    Delete image_scraping.py

--- image_scraping.py
+++
@@ -1,4 +0,0 @@
-from bing_image_downloader import downloader
-query_string = input("enter your search keyword:")
-x = int(input("number of images you want:"))
-downloader.download(query_string, limit= x,  output_dir='dataset', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
